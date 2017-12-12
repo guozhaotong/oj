a0538a319429dad42b77f5d929739da9d8d0a712
guozhaotong <xjtuguozhaotong@qq.com>
2017-12-12 22:37:21 +0800

    50

---
+++ src/main/java/medium/M050_powXN.java
@@ -0,0 +1,11 @@
+package medium;
+
+/**
+ * @author 郭朝彤
+ * @date 2017/12/12.
+ */
+public class M050_powXN {
+    public static void main(String[] args){
+        System.out.println(Math.pow(0,0));
+    }
+}
