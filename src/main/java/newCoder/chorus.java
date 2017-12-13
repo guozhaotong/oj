d41256a9a461c9341fdba5335306641018f0d55a
guozhaotong <xjtuguozhaotong@qq.com>
2017-12-13 22:41:04 +0800

    牛客  合唱团

---
+++ src/main/java/newCoder/chorus.java
@@ -0,0 +1,83 @@
+package newCoder;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author 郭朝彤
+ * @date 2017/12/13.
+ */
+public class chorus {
+    public static int maxProduct(int n, int[] singer, boolean[] valueFind, int k, int d) {
+        if (k > n) {
+            return 0;
+        }
+        int product = 1;
+        if (n - k + 1 <= d) {
+            Arrays.sort(singer);
+            for (int i = n - 1; i > n - k - 1; i--) {
+                product = product * singer[i];
+            }
+            return product;
+        }
+        int max = 0;
+        int lastAb = singer[0];
+        for (int i = 0; i < n - k + 1; i++) {
+            if(!valueFind[i] && singer[i] < lastAb){
+                continue;
+            }
+            lastAb = singer[i];
+            max = Math.max(max, maxPro(k,n,d,singer,i,valueFind));
+        }
+
+        return max;
+    }
+
+    public static int maxPro (int k, int n,  int d, int[] singer, int beginIndex, boolean[] valueFind){
+        int product = singer[beginIndex];
+        for (int t = 2; t <= k; t++) {
+            if (n - beginIndex - t + 1 <= d) {
+                int subSinger[] = new int[n - beginIndex];
+                System.arraycopy(singer, beginIndex, subSinger, 0, n - beginIndex);
+                Arrays.sort(subSinger);
+                for (int h = n - beginIndex - 1; h >= n - beginIndex - k + t - 1; h--) {
+                    product = product * subSinger[h];
+                }
+                break;
+            }
+            beginIndex = maxIndex(singer, beginIndex, d);
+            valueFind[beginIndex] = false;
+            product = product * singer[beginIndex];
+        }
+        int max = 0;
+        if(product > max){
+            max = product;
+        }
+        return max;
+    }
+
+    public static int maxIndex(int[] singer, int curIndex, int d) {
+        int max = Integer.MIN_VALUE;
+        int maxI = 0;
+        for (int j = curIndex + d; j >= curIndex; j--) {
+            if (singer[j] > max) {
+                max = singer[j];
+                maxI = j;
+            }
+        }
+        return maxI;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] singer = new int[n];
+        boolean[] valueFind = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            singer[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int d = sc.nextInt();
+
+    }
+}
